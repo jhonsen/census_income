5b18d7398063f54d71d356e68686b1ed01a4e8f0
Jhonsen Djajamuliadi <jhonsky@gmail.com>
2022-09-14 21:49:00 -0700

    Add modeling step

--- src/modeling/model.py
+++ src/modeling/model.py
@@ -1,5 +1,11 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from imblearn.pipeline import Pipeline as imbPipeline
+from imblearn.combine import SMOTEENN
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,7 +23,40 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    # select features
+    capital_diff_feature = ['capital_diff']
+    numeric_features = ['age','fnlgt','education_num','hours_per_week']
+    categorical_features = ['workclass', 'marital_status', 'occupation', 'relationship',
+        'race', 'sex', 'native_country']
+
+
+    # Setup transformers
+    numeric_transformer = make_pipeline(SimpleImputer())
+    categorical_transformer = make_pipeline(OneHotEncoder(drop='first',handle_unknown='ignore'))
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('numeric', numeric_transformer, numeric_features+capital_diff_feature),
+            ("categorical", categorical_transformer, categorical_features),
+        ],
+        remainder="passthrough",  # This drops the columns that we do not transform
+    )
+
+    classifier = RandomForestClassifier(n_jobs=-1)
+
+    train_cols = numeric_features + categorical_features + ['capital_diff']
+
+    model = imbPipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("resampler",  SMOTEENN(n_jobs=-1, random_state=42)),
+            ("classifier", classifier),
+        ]
+    )
+
+    model.fit(X_train[train_cols], y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -45,7 +84,7 @@ def inference(model, X):
     """ Run model inferences and return the predictions.
     Inputs
     ------
-    model : ???
+    model : Random Forest Classifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -54,4 +93,6 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    predictions = model.predict(X)
+    
+    return predictions
