92acf86c37227c1f6c57d7f80edf10f7dd93c323
Jhonsen Djajamuliadi <jhonsky@gmail.com>
2022-09-15 20:39:23 -0700

    Add data preparation script

--- src/preprocessing/data_processing.py
+++ src/preprocessing/data_processing.py
@@ -1,66 +1,59 @@
+import os
+import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
+DATAFOLDER = 'data'
 
-def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
-):
-    """ Process the data used in the machine learning pipeline.
-    Processes the data using one hot encoding for the categorical features and a
-    label binarizer for the labels. This can be used in either training or
-    inference/validation.
-    Note: depending on the type of model used, you may want to add in functionality that
-    scales the continuous data.
-    Inputs
-    ------
-    X : pd.DataFrame
-        Dataframe containing the features and label. Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
-    label : str
-        Name of the label column in `X`. If None, then an empty array will be returned
-        for y (default=None)
-    training : bool
-        Indicator if training mode or inference/validation mode.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
-    Returns
-    -------
-    X : np.array
-        Processed data.
-    y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
-        in.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained LabelBinarizer if training is True, otherwise returns the binarizer
-        passed in.
-    """
+def load_data(filepath):
+    data = pd.read_csv(filepath)
+    return data
 
-    if label is not None:
-        y = X[label]
-        X = X.drop([label], axis=1)
-    else:
-        y = np.array([])
+def fix_column_names(data):
 
-    X_categorical = X[categorical_features].values
-    X_continuous = X.drop(*[categorical_features], axis=1)
+    # Fix colnames for pandas
+    colnames = [c.replace('-','_').strip() for c in data.columns]
+    data.columns = colnames
 
-    if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
-        lb = LabelBinarizer()
-        X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y.values).ravel()
-    else:
-        X_categorical = encoder.transform(X_categorical)
-        try:
-            y = lb.transform(y.values).ravel()
-        # Catch the case where y is None because we're doing inference.
-        except AttributeError:
-            pass
+    return data
 
-    X = np.concatenate([X_continuous, X_categorical], axis=1)
-    return X, y, encoder, lb
+def preprocess_data(data):
+    
+    categorical_features = [
+        'workclass',
+        'education',
+        'marital_status',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'native_country',
+        'salary'
+    ]
+    numerical_features = [
+        'age',
+        'fnlgt',
+        'education_num',
+        'capital_gain',
+        'capital_loss',
+        'hours_per_week'
+    ]
+    
+    data = fix_column_names(data)
+
+    # replace nans with mean
+    for cat in numerical_features:
+        data[cat].fillna(data[cat].mean(), inplace=True)
+
+    # replace nans with mode
+    for cat in categorical_features:
+        data[cat] = data[cat].apply(lambda s: s.strip())
+        data.loc[data[cat]=="?", cat] = np.nan
+        data[cat].fillna(data[cat].mode().values[0], inplace=True)        
+
+    return data
+
+if __name__ == "__main__":
+
+    census_data = load_data(os.path.join(DATAFOLDER, 'census.csv'))
+    clean_data = preprocess_data(census_data)
+    clean_data.to_csv(os.path.join(DATAFOLDER, 'clean_census.csv'))
