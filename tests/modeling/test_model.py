b140333d7d7567b5ca042f1e863534de7ae88bd2
Jhonsen Djajamuliadi <jhonsky@gmail.com>
2022-09-21 21:08:43 -0700

    Add more test_model

--- tests/modeling/test_model.py
+++ tests/modeling/test_model.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import pytest
+import gzip
+import pickle
 import pandas as pd
 CWD = os.getcwd()
 sys.path.append('{}/src'.format(CWD))
-from src.modeling.model import feature_engineer
+from src.modeling.model import feature_engineer, inference
+from src.modeling.constants import MODELFOLDER, DATAFOLDER
 
 @pytest.fixture(scope="session")
 def clean_data():
@@ -14,7 +17,30 @@ def clean_data():
 
     return df
 
+@pytest.fixture(scope="session")
+def holdout_data():
+
+    with gzip.open(os.path.join(DATAFOLDER, 'X_test.pkl'),'rb') as fin:
+        X_test = pickle.load(fin)
+    
+    return X_test
+
+@pytest.fixture(scope="session")
+def model():
+
+    with gzip.open(os.path.join(MODELFOLDER, 'rf_model.p'),'rb') as fin:
+        model = pickle.load(fin)
+
+    return model
+
 def test_feature_engineer_output(clean_data):
 
     cleaned_data = feature_engineer(clean_data)
     assert 'capital_diff' in cleaned_data.columns
+
+def test_inference(model, holdout_data):
+
+    pred_array = inference(model, holdout_data)
+    prediction = pred_array[0]
+    assert prediction in (['>50K','<=50K'])
+
